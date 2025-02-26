072671cd05c743bbaea396f9c83faf8cf03a8307
Disha Choubey <112333295+dishaaach03@users.noreply.github.com>
2025-02-27 00:31:46 +0530

    Add files via upload

---
+++ DAY3/validatorTest.java
@@ -0,0 +1,24 @@
+package sample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class validatorTest {
+
+    @Test
+    void isPrime() {
+    }
+
+    @Test
+    void isPalindrome() {
+    }
+
+    @Test
+    void isValidUsername() {
+    }
+
+    @Test
+    void isValidPassword() {
+    }
+}
